b67e9f2d2022a492d5e81329f60309a84541cb7c
윤현준 <homelus@daum.net>
2019-10-15 13:32:01 +0900

    refactor: mvc config

--- src/main/java/study/mvc/config/MvcConfiguration.java
+++ src/main/java/study/mvc/config/MvcConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import study.mvc.interceptor.TestInterceptor;
 
 /**
@@ -16,7 +16,7 @@ import study.mvc.interceptor.TestInterceptor;
 @ComponentScan("study")
 @EnableWebMvc
 @ImportResource("classpath:mvc-config.xml")
-public class MvcConfiguration extends WebMvcConfigurationSupport {
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TestInterceptor());
