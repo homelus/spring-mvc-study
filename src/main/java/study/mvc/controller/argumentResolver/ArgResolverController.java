437c97a6a45d6718a8e1d32b668dbde3b943c0b3
윤현준 <homelus@daum.net>
2019-10-29 12:47:47 +0900

    feat: arg resolver

--- src/main/java/study/mvc/controller/argumentResolver/ArgResolverController.java
+++ src/main/java/study/mvc/controller/argumentResolver/ArgResolverController.java
@@ -1,5 +1,6 @@
 package study.mvc.controller.argumentResolver;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import study.mvc.model.TmsUser;
 
 import java.util.HashMap;
@@ -17,6 +21,20 @@ import java.util.HashMap;
  */
 @RestController
 public class ArgResolverController {
+
+    /**
+     * 순수한 파라미터인 경우 어떻게 처리 될 것인가?
+     *
+     * String 인 경우, int 인 경우
+     * 
+     * @see org.springframework.web.method.annotation.RequestParamMethodArgumentResolver
+     * 
+     * @see org.springframework.beans.BeanUtils#isSimpleProperty(Class)
+     */
+    @RequestMapping("/api/t0/tmsUser")
+    public String t1(String name, int age) {
+        return "name: " + name + " age: " + age;
+    }
 
     /**
      * @see org.springframework.web.servlet.mvc.method.annotation.ServletModelAttributeMethodProcessor
@@ -34,6 +52,23 @@ public class ArgResolverController {
     @RequestMapping("/api/t2/tmsUser")
     public String t2(@RequestParam Integer count) {
         return count.toString();
+    }
+
+    /**
+     * 파라미터가 2개인 경우
+     *
+     * RequestParam 의 설정값(value, required)에 따라서 어떻게 처리될까?
+     *
+     * @see org.springframework.web.method.annotation.RequestParamMethodArgumentResolver
+     *
+     * @see org.springframework.web.method.annotation.RequestParamMethodArgumentResolver#resolveArgument(MethodParameter, ModelAndViewContainer, NativeWebRequest, WebDataBinderFactory) 
+     * 
+     * @see org.springframework.web.method.annotation.RequestParamMethodArgumentResolver#createNamedValueInfo(MethodParameter)
+     * @see org.springframework.web.method.annotation.RequestParamMethodArgumentResolver.RequestParamNamedValueInfo
+     */
+    @RequestMapping("/api/t2_2/tmsUser")
+    public String t2_2(@RequestParam(value = "cnt") Integer count, @RequestParam(required = false) String arg) {
+        return count.toString() + " " + arg;
     }
 
     // 혼합 테스트
